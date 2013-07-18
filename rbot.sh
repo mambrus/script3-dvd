e3607efe4dd043a302c614519cc011825a3c5795
Michael Ambrus <michael.ambrus@gmail.com>
2013-07-18 07:23:20 +0200

    rbot.sh added
    Automates process making console obsolete after start.
    
    Minor fix in toiso making automating process able to continue even if
    previous run failed.
    
    TBD:
    * Make it daemonizeable
    
    Change-Id: Id084fcf9ea0a3dae5b247a51137ebd42b28a637d

---
+++ rbot.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Author: Michael Ambrus (ambrmi09@gmail.com)
+# 2013-07-18
+
+if [ -z $RBOT_SH ]; then
+
+RBOT_SH="rbot.sh"
+
+TMP_OUT_FILE="/tmp/rbot.txt"
+
+
+function rbot() {
+	echo "Creating ${RIPDIR}/${PROJECT}"
+	mkdir "${RIPDIR}/${PROJECT}"
+	echo "Reading DVD into [${RIPDIR}/${PROJECT}]"
+	rm -f $TMP_OUT_FILE
+	touch $TMP_OUT_FILE
+
+	# Make a full mirror. Any copy-protection will still leave us with
+	# enough VOBS to to create our own main-feature (which can me authored if
+	# needed)
+	if [ "X${VERBOSE}" == "Xyes" ]; then
+		time dvdbackup -i /dev/dvd -M -p -o "${RIPDIR}/${PROJECT}"
+	else
+		time (dvdbackup -i /dev/dvd -M -p -o "${RIPDIR}/${PROJECT}" >> \
+			$TMP_OUT_FILE 2>&1 )
+	fi
+	eject
+	if [ "X${PROJECT}" == "X${DEF_PROJ}" ]; then
+		#No project given on command-line. Detect a new one based on the
+		#auto-detected feature-name
+		NEW_PROJ=$(ls ${RIPDIR}/${PROJECT} | \
+			sed -e 's/[[:space:]]\+/_/g' | \
+			tr '[:lower:]' '[:upper:]' )
+		echo "New project-name detected..."
+		echo "Renaming [${RIPDIR}/${PROJECT}] to [${RIPDIR}/${NEW_PROJ}]"
+		mv "${RIPDIR}/${PROJECT}" "${RIPDIR}/${NEW_PROJ}"
+		PROJECT="${NEW_PROJ}"
+	fi
+
+	#Will spaces be handled correctly?
+	FEATURE_CREATED=$(ls ${RIPDIR}/${PROJECT})
+
+	if [ "X${ISO}" == "X${DEF_ISO}" ]; then
+		#No specific iso-file-name given. Autodetect one
+		
+		NEW_ISO="$(ls ${RIPDIR}/${PROJECT} | \
+			sed -e 's/[[:space:]]\+/_/g').iso"
+		ISO="${NEW_ISO}"
+		echo "New ISO file-name detected [${ISO}]"
+	fi
+
+	if ! [ -d "${RIPDIR}/${PROJECT}/${FEATURE_CREATED}/AUDIO_TS" ]; then
+		echo "Missing [${RIPDIR}/${PROJECT}/${FEATURE_CREATED}/AUDIO_TS]"
+		echo " ...creating phony directory."
+		mkdir "${RIPDIR}/${PROJECT}/${FEATURE_CREATED}/AUDIO_TS"
+	fi
+
+	(
+		cd "${RIPDIR}/${PROJECT}"
+
+		echo "Creating iso-file [${ISO}] from"\
+			"[${RIPDIR}/${PROJECT}/${FEATURE_CREATED}]"
+		if [ "X${VERBOSE}" == "Xyes" ]; then
+			time genisoimage -dvd-video -iso-level 2 -o "${ISO}" \
+				"${RIPDIR}/${PROJECT}/${FEATURE_CREATED}"
+		else
+			time (genisoimage -dvd-video -iso-level 2 -o "${ISO}" \
+				"${RIPDIR}/${PROJECT}/${FEATURE_CREATED}" >> $TMP_OUT_FILE 2>&1)
+		fi
+	)
+	if [ "X${KEEP}" == "Xno" ]; then
+		mv "${RIPDIR}/${PROJECT}/${ISO}" "${RIPDIR}"
+		rm -rf "${RIPDIR}/${PROJECT}"
+	fi
+}
+
+source s3.ebasename.sh
+if [ "$RBOT_SH" == $( ebasename $0 ) ]; then
+	#Not sourced, do something with this.
+
+	RBOT_SH_INFO="dvd.${RBOT_SH}"
+	source .dvd.ui..rbot.sh
+
+	set -u
+	set -e
+	echo "${RBOT_SH_INFO} started: $(date +"%D %T")"
+	time rbot "$@"
+	echo "${RBOT_SH_INFO} stopped: $(date +"%D %T")"
+	which play && play ~/bin/.dvd..alert.wav >/dev/null 2>&1
+	RC=$?
+
+	exit $RC
+fi
+
+fi
